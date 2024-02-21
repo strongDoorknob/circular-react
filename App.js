e782c4138e99ac9e14a36aee666bc86cf071cb3f
Nick <iamnicolus@gmail.com>
2024-02-21 22:46:00 +0700

    fking tireddddd

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Home } from './app/index.js';
 
 export default function App() {
   return (
---
+++ app/index.js
@@ -0,0 +1,45 @@
+import { View, ScrollView, SafeAreaView } from 'react-native';
+import { useState } from 'react';
+import { Stack, useRouter } from 'expo-router';
+
+import { COLORS, icons, images, SIZES } from '../constants';
+import { Nearbyjobs, PopularJobs, ScreenHeaderBtn, Welcome } from '../components';
+
+
+const Home = () => {
+    const router = useRouter();
+
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+            <Stack.Screen
+                options={{
+                    headerStyle: { backgroundColor: COLORS.lightWhite },
+                    headerShadowVisible: false,
+                    headerLeft: () => {
+                        <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%"/>
+                    },
+                    headerRight: () => {
+                        <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%"/>
+                    },
+                    headerTitle: ""
+                }}
+            />
+
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View
+                    style={{
+                        flex: 1,
+                        padding: SIZES.medium
+                    }}
+                >
+                    <Welcome 
+                        
+                    />
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    )
+}
+
+export default Home;
+
